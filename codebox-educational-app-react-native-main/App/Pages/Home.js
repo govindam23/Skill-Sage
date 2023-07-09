6da831ea89c280fbcd5b97d2632cb14d5d472c36
rajdeep toor <toor81@uwindsor.ca>
2023-07-08 23:39:08 -0400

    added trial code for chatbox

--- codebox-educational-app-react-native-main/App/Pages/Home.js
+++ codebox-educational-app-react-native-main/App/Pages/Home.js
@@ -1,15 +1,11 @@
-import { View, Text } from 'react-native'
-import React, { useContext, useEffect } from 'react'
-import { Button } from 'react-native'
-import Services from '../Shared/Services'
-import { AuthContext } from '../Context/AuthContext'
-import WelcomeHeader from '../Components/WelcomeHeader'
+import React, { useContext } from 'react'
+import { ScrollView, View } from 'react-native'
+import CourseList from '../Components/CourseList'
 import SearchBar from '../Components/SearchBar'
-import GlobalApi from '../Shared/GlobalApi'
 import Slider from '../Components/Slider'
 import VideoCourseList from '../Components/VideoCourseList'
-import CourseList from '../Components/CourseList'
-import { ScrollView } from 'react-native'
+import WelcomeHeader from '../Components/WelcomeHeader'
+import { AuthContext } from '../Context/AuthContext'
 
 
 
