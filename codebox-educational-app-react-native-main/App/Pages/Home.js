b669712dfbd030ebfe2062dc1f0d41d3263395cc
rajdeep toor <toor81@uwindsor.ca>
2023-07-16 16:14:30 -0400

    added async.clear(logout) button

--- codebox-educational-app-react-native-main/App/Pages/Home.js
+++ codebox-educational-app-react-native-main/App/Pages/Home.js
@@ -1,19 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useContext } from 'react';
-import { ScrollView, View } from 'react-native';
+import { Button, ScrollView, View } from 'react-native';
 import CourseList from '../Components/CourseList';
-
 
 import SearchBar from '../Components/SearchBar';
 import Slider from '../Components/Slider';
 import VideoCourseList from '../Components/VideoCourseList';
 import WelcomeHeader from '../Components/WelcomeHeader';
 import { AuthContext } from '../Context/AuthContext';
+
  
 
 
 export default function Home() {
     const {userData,setUserData}=useContext(AuthContext)
-     
+    
+const Logout=()=>{
+  AsyncStorage.clear()
+  setUserData(null);
+}
+  
   return (
     <ScrollView style={{padding:20}}>
       <View style ={{fontFamily:''}}>
@@ -26,8 +32,11 @@ export default function Home() {
         
       
         <View style={{height:110}}> 
+        <Button title="Logout" onPress={Logout} />
         </View>
+        
         </View>
+        
     </ScrollView> 
     
   )
